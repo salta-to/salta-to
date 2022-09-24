bbc4a34e7852f8bc6de25fb824c2763550ff570c
Joop Laan <joop.laan@gmail.com>
2022-09-24 08:39:33 +0200

    Add clamp font size calculator

---
+++ calculate-font-size-clamp/precache-manifest.684b4d99c16d57c90d2d7d2b8167b1db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f39a1bb066fb04a25a81ec339bc002",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d3ae8cba0e377b27025",
+    "url": "/static/css/main.3ddc8ce3.chunk.css"
+  },
+  {
+    "revision": "bc4295739d3060daed9e",
+    "url": "/static/js/2.9d98b648.chunk.js"
+  },
+  {
+    "revision": "7d3ae8cba0e377b27025",
+    "url": "/static/js/main.ddf56f4e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "87296f22099264737ae19173ae9a7a08",
+    "url": "/static/media/copy.87296f22.svg"
+  }
+]);
