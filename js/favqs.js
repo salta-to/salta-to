ee92cfd33b386558fffa2c58d33f4ca5f7746f2d
Joop Laan <joop.laan@gmail.com>
2023-07-02 08:55:44 +0200

    Clean up

--- js/favqs.js
+++ js/favqs.js
@@ -1,67 +1,11 @@
-async function getMyFavQuotes() {
-  let divQotd = document.querySelector('#quote');
-  let url = 'https://favqs.com/api/quotes/?filter=joop&type=user';
+const api_url ="https://zenquotes.io/api/quotes/";
 
-  utils.getJSON(url).then(function (quotes) {
-    let randQuote = randomQuote(quotes['quotes']);
-    divQotd.innerHTML = displayQuote(randQuote);
-  }).catch(function (e) {
-    console.error('error - ' + e);
-  });
+async function getapi(url)
+{
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log('xxxx');
+  console.log(data);
 }
 
-function displayQuote(quote) {
-  let html = ''
-  html += '<blockquote cite="' + quote['url'] + '">';
-  html += '<p>' + quote['body'] + '</p>';
-  html += '<cite>' + quote['author'] + '</cite>';
-  html += '</blockquote>';
-  return html;
-}
-
-function randomQuote(obj) {
-  var keys = Object.keys(obj);
-  return obj[keys[keys.length * Math.random() << 0]];
-}
-
-let utils = {};
-utils.get = (url) => {
-  return new Promise(function (resolve, reject) {
-    let req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.setRequestHeader('Authorization', 'Token token="25b82847cd840f5905f6bceffac0aaf2"');
-    req.onload = function () {
-      if (req.status == 200) {
-        resolve(req.response);
-      }
-      else {
-        reject(Error('promise error with ' + req.status));
-      }
-    };
-    req.onerror = function (err) {
-      reject(Error('Network Error with ' + url + ': ' + err));
-    };
-    req.send();
-  });
-}
-
-utils.getJSON = async function (url) {
-  var data = {};
-  var string = null;
-  try {
-    string = await utils.get(url);
-  }
-  catch (e) {
-    console.error('error: ' + e);
-  }
-  try {
-    data = JSON.parse(string);
-    success = true;
-  }
-  catch (e) {
-    console.error('parse error');
-  }
-  return data;
-}
-
-getMyFavQuotes();
+getapi(api_url);
